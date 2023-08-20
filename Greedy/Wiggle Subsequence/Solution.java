a8fcafe96f6a494f6fbf50eda542efae86e09b78
shubhamjaggi <shubham.jaggi@gmail.com>
2023-08-20 13:04:13 +0530

    improved solution for 'Wiggle Subsequence'

--- Greedy/Wiggle Subsequence/Solution.java
+++ Greedy/Wiggle Subsequence/Solution.java
@@ -1,29 +1,14 @@
 class Solution {
     public int wiggleMaxLength(int[] nums) {
-        if(nums.length == 1) return 1;
-
-        int count = 1;
-        int i = 1;
-
-        while(i < nums.length && nums[i] == nums[i - 1]) i++;
-
-        if(i < nums.length) {
-            int prevDiff = nums[i] - nums[i - 1];
-
-            count++;
-            i++;
-
-            for(; i < nums.length; i++) {
+            Integer prevDiff = null;
+            int count = 1;
+            for(int i = 1; i < nums.length; i++) {
                 int currDiff = nums[i] - nums[i - 1];
                 if(currDiff != 0){
-                    if((prevDiff < 0 && currDiff > 0) || (prevDiff > 0 && currDiff < 0)) count++;
+                    if((prevDiff == null) || (prevDiff < 0 && currDiff > 0) || (prevDiff > 0 && currDiff < 0)) count++;
                     prevDiff = currDiff;
                 }
             }
-
             return count;
-        }else{
-            return 1;
-        }
     }
 }
