04a79ce777a5599ba5d7d9d61405109ae71d7d6d
shubhamjaggi <shubham.jaggi@gmail.com>
2025-09-08 00:22:55 +0530

    update solution for 'Valid Parentheses'

--- Stack/Valid Parentheses/Solution.java
+++ Stack/Valid Parentheses/Solution.java
@@ -1,34 +1,33 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == '{' || s.charAt(i) == '[' || s.charAt(i) == '(') {
-                stack.push(s.charAt(i));
-            }
-            else if(!stack.isEmpty()) {
-                if(s.charAt(i)=='}') {
-                    if (stack.peek()=='{') {
-                        stack.pop();
-                    }
-                    else return false;
-                }
-                else if(s.charAt(i)==']') {
-                    if (stack.peek()=='[') {
-                        stack.pop();
-                    }
-                    else return false;
-                }
-                else if(s.charAt(i)==')') {
-                    if (stack.peek()=='(') {
-                        stack.pop();
-                    }
-                    else return false;
+        int n=s.length();
+        Stack<Character> config=new Stack<>();
+
+        for(int i=0; i<n; i++) {
+            char curr=s.charAt(i);
+            if(isOpen(curr)) {
+                config.push(curr);
+            } else {
+                if(config.isEmpty()) return false;
+                if(config.peek()==getOpening(curr)) {
+                    config.pop();
+                } else {
+                    return false;
                 }
             }
-            else return false;
         }
 
-        if (!stack.isEmpty()) return false;
-        return true;
+        return config.isEmpty();
+    }
+
+    private boolean isOpen(char ch) {
+        return (ch=='(' || ch=='[' || ch=='{');
+    }
+
+    private char getOpening(char ch) {
+        if(ch==')') return '(';
+        if(ch==']') return '[';
+        if(ch=='}') return '{';
+        return ' ';
     }
 }
