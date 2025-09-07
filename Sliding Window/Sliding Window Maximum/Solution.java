023d643d9a1e9a8f503dcbb848e0c445ad318a7e
shubhamjaggi <shubham.jaggi@gmail.com>
2025-09-07 15:13:31 +0530

    refactor solution for 'Sliding Window Maximum'

--- Sliding Window/Sliding Window Maximum/Solution.java
+++ Sliding Window/Sliding Window Maximum/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         Deque<Integer> candidates = new LinkedList<>();
         int ans[] = new int[n-k+1]; // there will be exactly n-k+1 windows
 
-        for(int i=0; i<k; i++) onInclusionToCurrentWindow(candidates, i, nums); // prepare candidates for first window
+        for(int i=0; i<k; i++) updateCandidatesOnAddition(candidates, i, nums); // prepare candidates for first window
 
         // at every window, SWM will be nums[candidates.peekFirst()]
         ans[0]=nums[candidates.peekFirst()];
@@ -13,8 +13,8 @@ class Solution {
             int toBeRemoved=i-1;
             int toBeAdded=i+k-1;
 
-            onRemovalFromCurrentWindow(candidates, toBeRemoved, nums);
-            onInclusionToCurrentWindow(candidates, toBeAdded, nums);
+            if(candidates.peekFirst()==toBeRemoved) candidates.poll();
+            updateCandidatesOnAddition(candidates, toBeAdded, nums);
 
             // at every window, SWM will be nums[candidates.peekFirst()]
             ans[toBeAdded-k+1]=nums[candidates.peekFirst()];
@@ -23,11 +23,8 @@ class Solution {
         return ans;
     }
 
-    private void onRemovalFromCurrentWindow(Deque<Integer> candidates, int toBeRemoved, int[] nums) {
-        if(nums[candidates.peekFirst()]==nums[toBeRemoved]) candidates.poll();
-    }
-
-    private void onInclusionToCurrentWindow(Deque<Integer> candidates, int toBeAdded, int[] nums) {
+    // runs on every inclusion to the current window
+    private void updateCandidatesOnAddition(Deque<Integer> candidates, int toBeAdded, int[] nums) {
         // remove all elements from the last that are smaller than the element whose index is to be added
         while(!candidates.isEmpty() && nums[candidates.peekLast()]<nums[toBeAdded]) candidates.removeLast();
 
