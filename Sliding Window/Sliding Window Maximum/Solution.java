964cc18e31d294217de87758fbcaf8fe75dbbf90
shubhamjaggi <shubham.jaggi@gmail.com>
2025-09-07 15:04:57 +0530

    update solution for 'Sliding Window Maximum'

--- Sliding Window/Sliding Window Maximum/Solution.java
+++ Sliding Window/Sliding Window Maximum/Solution.java
@@ -1,25 +1,37 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> dq = new LinkedList<>();
+        int n=nums.length;
+        Deque<Integer> candidates = new LinkedList<>();
+        int ans[] = new int[n-k+1]; // there will be exactly n-k+1 windows
 
-        for(int i = 0; i < k; i++) {
-            while(!dq.isEmpty() && dq.peekLast() < nums[i]) dq.removeLast();
-            dq.offerLast(nums[i]);
-        }
+        for(int i=0; i<k; i++) onInclusionToCurrentWindow(candidates, i, nums); // prepare candidates for first window
 
-        int[] ans = new int[nums.length - k + 1];
+        // at every window, SWM will be nums[candidates.peekFirst()]
+        ans[0]=nums[candidates.peekFirst()];
 
-        ans[0] = dq.peekFirst();
-        
-        for(int i = k, j = 0; i < nums.length; i++, j++) {
-            if (dq.peekFirst() == nums[j]) {
-                dq.poll();
-            }
-            while(!dq.isEmpty() && dq.peekLast() < nums[i]) dq.removeLast();
-            dq.offerLast(nums[i]);
-            ans[i - k + 1] = dq.peekFirst();
+        for(int i=1; i<(n-k+1); i++) {
+            int toBeRemoved=i-1;
+            int toBeAdded=i+k-1;
+
+            onRemovalFromCurrentWindow(candidates, toBeRemoved, nums);
+            onInclusionToCurrentWindow(candidates, toBeAdded, nums);
+
+            // at every window, SWM will be nums[candidates.peekFirst()]
+            ans[toBeAdded-k+1]=nums[candidates.peekFirst()];
         }
 
         return ans;
+    }
+
+    private void onRemovalFromCurrentWindow(Deque<Integer> candidates, int toBeRemoved, int[] nums) {
+        if(nums[candidates.peekFirst()]==nums[toBeRemoved]) candidates.poll();
+    }
+
+    private void onInclusionToCurrentWindow(Deque<Integer> candidates, int toBeAdded, int[] nums) {
+        // remove all elements from the last that are smaller than the element whose index is to be added
+        while(!candidates.isEmpty() && nums[candidates.peekLast()]<nums[toBeAdded]) candidates.removeLast();
+
+        // now add the index toBeAdded
+        candidates.offerLast(toBeAdded);
     }
 }
