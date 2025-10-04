d4cfe59d1d7dcc3afac08b87fa47424d27a5dc3c
shubhamjaggi <shubham.jaggi@gmail.com>
2025-10-05 01:46:36 +0530

    add solution for 'Validate Binary Search Tree'

---
+++ Tree/Validate Binary Search Tree/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long min, long max) {
+        if(root==null) return true;
+        if(root.val<=min || root.val>=max) return false;
+        return (root.left==null || root.val>root.left.val) && (root.right==null || root.val<root.right.val) &&
+            isValidBST(root.left,min,root.val) && isValidBST(root.right,root.val,max);
+    }
+}
