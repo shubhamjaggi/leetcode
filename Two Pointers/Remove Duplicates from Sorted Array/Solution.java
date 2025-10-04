f9d031b84f56d146ed58d32f94d14087b4f32377
shubhamjaggi <shubham.jaggi@gmail.com>
2025-10-04 13:14:52 +0530

    update solution for 'Remove Duplicates from Sorted Array'

--- Two Pointers/Remove Duplicates from Sorted Array/Solution.java
+++ Two Pointers/Remove Duplicates from Sorted Array/Solution.java
@@ -1,14 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int slow=0, fast=0;
-
-        while(fast<nums.length) {
-            if(slow==0 || nums[slow-1]!=nums[fast]) { // next unique number found
-                nums[slow++]=nums[fast];
+        int inIndex=0; // index where non-dupl vals will be added // 0th index will keep the exsiting value
+        for(int i=1;i<nums.length;i++) {
+            if(nums[i-1]<nums[i]) { // different val found
+                nums[++inIndex]=nums[i]; // inIndex is incremented when a new value is found
             }
-            fast++;
         }
-
-        return slow;
+        return inIndex+1; // +1 because num of vals needs to returned => not last input index (which is 0 based)
     }
 }
