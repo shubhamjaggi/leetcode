ecac9ac3c6c12e166b75f5f2f478a3ceecbfc60f
shubhamjaggi <shubham.jaggi@gmail.com>
2025-10-04 23:55:35 +0530

    add solution for 'Same Tree'

---
+++ Tree/Same Tree/Solution.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null) return q==null;
+        if(q==null) return p==null;
+        return p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+}
